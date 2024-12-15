408a916ae9c28ab61a01bce16d27dcfbada3566c
Harshit-1874 <harshit1084.be21@chitkarauniversity.edu.in>
2024-12-15 18:15:40 +0530

    login integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import { ReactFlowProvider  } from '@xyflow/react';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
@@ -9,37 +9,45 @@ import ForgotPassword from './pages/ForgotPassword';
 import CanvasPage from './pages/CanvasPage';
 import UserDashboard from './pages/UserDashboard';
 import PrivateRoute from './components/PrivateRoute';
+import AuthProvider from './context/authContext';
+
+const AuthProviderWrapper = ({ children }) => {
+  const navigate = useNavigate();
+  return <AuthProvider navigate={navigate}>{children}</AuthProvider>;
+};
 
 function App() {
   return (
-    <ReactFlowProvider >
-      <Router>
-        <div className="App" >
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route
-              path="/canvas"
-              element={
-                <PrivateRoute>
-                  <CanvasPage/>
-                </PrivateRoute>
-              } 
-            />
-            <Route path='/forgot-password' element={<ForgotPassword/>}/>
-            <Route
-              path='/dashboard'
-              element={
-                <PrivateRoute>
-                  <UserDashboard/>
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
-    </ReactFlowProvider >
+      <ReactFlowProvider >
+        <Router>
+          <AuthProviderWrapper>
+          <div className="App" >
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+              <Route
+                path="/canvas"
+                element={
+                  <PrivateRoute>
+                    <CanvasPage/>
+                  </PrivateRoute>
+                } 
+              />
+              <Route path='/forgot-password' element={<ForgotPassword/>}/>
+              <Route
+                path='/dashboard'
+                element={
+                  <PrivateRoute>
+                    <UserDashboard/>
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+          </AuthProviderWrapper>
+        </Router>
+      </ReactFlowProvider >
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
+import { loginUser } from '../api/users';
+import { AuthContext } from '../context/authContext';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    setError(null);
     try {
-      console.log(email);
-      const response = await axios.post('http://localhost:4000/api/users/login', { email, password });
-      console.log('Logged in:', response.data);
-      // Save the token in localStorage
-      localStorage.setItem('token', response.data.token);
-      // Redirect to dashboard
-      window.location.href = '/dashboard';
+      const response = await loginUser({email, password});
+      login(response.data.token);
+      navigate('/dashboard');
     } catch (err) {
       setError(err.response?.data?.message || 'Login failed');
     }
