c13bd7e3aabf05308252f3c28b2cf3a3999eacd8
Pulkit <pulkit.@Pulkits-MacBook-Air.local>
2024-12-19 21:09:13 +0530

    registration fix

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -276,15 +276,17 @@ function Register() {
                 <div className="flex items-center">
                   <div className="flex">
                     <input
-                      id="remember-me"
-                      name="remember-me"
+                      id="termsAccepted"
+                      name="termsAccepted"
                       type="checkbox"
                       className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                      checked={termsAccepted}
+                      onChange={handleChange}
                     />
                   </div>
                   <div className="ms-3">
                     <label
-                      htmlFor="remember-me"
+                      htmlFor="termsAccepted"
                       className="text-sm dark:text-white"
                     >
                       I accept the{" "}
